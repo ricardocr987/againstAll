2763d010c71a18d479b661a427a469160537dc9d
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-11-13 12:35:18 +0100

    weather to-do

--- src/AA_Weather.ts
+++ src/AA_Weather.ts
@@ -2,8 +2,8 @@ import { Paths } from './paths.js'
 import { existsSync, mkdirSync, readFileSync } from 'fs'
 import { format, Options } from 'prettier'
 import { Server, Socket } from 'net'
-import { WeatherInfo, WeatherI, WeatherEvents } from './types.js'
-
+import { EngineEvents, WeatherEvents, WeatherInfo } from './types.js'
+import { config } from './config.js'
 
 const options: Options = {
     semi: false,
@@ -16,49 +16,39 @@ const options: Options = {
     parser: 'json',
 } // necesario para darle el formato correcto al mapa a la hora de escribir el fichero json
 
-
 export class Weather{
-    public paths: Paths =new Paths(`./`)
-    public port: number
-    public io:  Server
-    public peticion: String = ''
-    public num: number
-    public city: string
-    public temperature: number
-    public weathers: Record<number, WeatherI> = this.getWeather()
-    public infoWeather: Record<string, WeatherInfo> = {}
+    public paths: Paths = new Paths(`./`) // is simply an object to make it easy to get the path to e.g. the database
+    
+    public io: Server // server instance
 
-    constructor(port: number) {
-        this.port = port
+    // MAPS
+    public cities: Record<number, string> = {} // key: number (id), value: city name
+    public infoWeather: Record<string, WeatherInfo> = {} // key: city name, value: weather of that city
+
+    constructor(        
+        public SERVER_PORT: number,
+    ) {
         this.io = new Server()
-        this.num = 0
-        this.city = ''
-        this.temperature = 0
     }
 
-    public getWeather(): Record<string, WeatherI> {
-        this.num = Math.floor(Math.random()*11)
-        //Buscar en el json la ciudad correspondiente al num aleatorio
-
-        if(!existsSync(this.paths.dataDir)) return {}// si no existe la carpeta data ...
-
-        const weathers: Record<number, WeatherInfo> = {}
-        const weather1: Record<number, WeatherInfo>  = JSON.parse(readFileSync(this.paths.dataFile("weather"), 'utf8'))
-        for(const weather of Object.values(weather1)){
-            weathers[weather.num] = weather
+    public getWeather() {
+        if(!existsSync(this.paths.dataFile('cities')) || !existsSync(this.paths.dataFile('weather'))) {
+            this.addCitiesNames()
+            this.addWeather()
         }
-
-        this.temperature = Math.floor(Math.random()* (Math.floor(10) - Math.ceil(-10)) + Math.ceil(-10))
-
-        return weathers
     }
 
-    public get weather(): WeatherInfo{
-        return {
-            num: this.num,
-            city: this.city,
-            temperature: this.temperature
-        }
+    public addCitiesNames () {
+        // crea un vector de ciudades ['Alicante',... ] por lo menos 15
+        // incluye esas ciudades en this.cities
+        // escribe un fichero llamado 'cities' con esa informacion como JSON, basate getPlayers() de registry (no hace falta que compruebes la existencia del archivo)
+        //         writeFileSync(this.paths.dataFile("registry"), format(JSON.stringify(this.registeredPlayers).trim(), options))
+
+    }
+
+    public addWeather () {
+        // for de this.cities, randomizas temperatura con randomIntFromInterval (final de este archivo)
+        // almacenas la info en this.infoWeather y escribes un fichero 'infoWeather' con la info del this.infoWeather
     }
 
     public Start(){
@@ -68,31 +58,25 @@ export class Weather{
             socket.setEncoding("utf-8") // cada vez que recibe un mensaje automaticamente decodifica el mensaje, convirtiendolo de bytes a un string entendible
         
             socket.on("data", (message) => { // cuando envias un mensaje desde el cliente, (socket.write) -> recibes un Buffer (bytes) que hay que decodificar, para convertirlo en string .toString()
-                const [event, alias] = message.toString().split(':') // creamos un vector de la respuesta del cliente con las tres variables
-                
-               /* const weatherInfo: WeatherInfo = { 
-                    num,
-                    city,
-                    temperature = Math.random()*20
-                }
-                */
-                switch(event){
-                    case WeatherEvents.ASK:
-                        try{
-                            this.getWeather()
-                        }catch(e){
-                            socket.write(`${WeatherEvents.ASK_ERROR}:${e}`)
-                        }
+                switch(message.toString()){
+                    case EngineEvents.GET_CITY_INFO:
+                        // envia solo una ciudad random: crea una funcion que te de numero aleatorio, sacas el nombre de la ciudad con ese numero con el map y con el nombre de la ciudad sacas la temperatura con el otro map
+                        // socket.write(`${WeatherEvents.WEATHER}:${cityName}:${cityWeather}`)
                         break
                 }
             })
         })
-        this.io.listen(this.port) // el servidor escucha el puerto 
+        this.io.listen(this.SERVER_PORT) // el servidor escucha el puerto 
     }
 
+    public randomIntFromInterval(min: number, max: number) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 }
+
 function main() {
-    const PORT = 1364
-    new Weather(Number(PORT)).Start()
+    const WEATHER_SERVER_PORT = Number(config.ENGINE_SERVER_PORT) || 5670
+    new Weather(WEATHER_SERVER_PORT).Start()
 }
+
 main()
