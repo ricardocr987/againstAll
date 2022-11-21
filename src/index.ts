2b2c07722df70f9d3ee74e97490fed18200e4d5b
abt39-ua <abt39@gcloud.ua.es>
2022-11-21 17:33:38 +0100

    Creación express, morgan e index

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+//export class index
+var express = require('express')
+var app = express();
+var morgan = require('morgan')
+
+
+app.set('port', process.env.PORT || 3000)
+app.set('json spaces', 2)
+
+app.use(morgan('dev'))
+app.use(express.urlenconded({extended:false}))
+app.use(express.json())
+
+
+app.get('/', (req: any, res: any) => {
+    res.json(
+        {
+            "Title": "Hola Mundo"
+        }
+    )
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Server listening on port ${app.get('port')}`)
+})
